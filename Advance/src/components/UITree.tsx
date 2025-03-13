04597b1c41a762de3cdbb7c6dfdb8ea415c1c332
Jason Ma <jasonma062019@gmail.com>
2025-03-13 00:11:08 -0700

    Render Tree and Module Dependency Tree in React

---
+++ Advance/src/components/UITree.tsx
@@ -0,0 +1,7 @@
+// Render Tree
+// Representing the structure of React components as a tree is called Render Tree.
+
+// Module Dependency Tree
+// An app's module dependencies is where we may export components, functions, or constants.
+// 1. Each node in a module dependency tree is a module 
+// 2. Each branch represents an import statement in that module.
