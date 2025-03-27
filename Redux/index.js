875b29233f66a8964eed3ea6aa5c282042a7f231
Jason Ma <jasonma062019@gmail.com>
2025-03-27 12:40:08 -0700

    Basic concepts of redux in vanilla javascript with node.js

---
+++ Redux/index.js
@@ -0,0 +1,51 @@
+// A store that holds the state of your application.
+// An action that describes the changes in the state of the application.
+// A reducer which actually carries out the state transition depending on the action.
+const redux = require('redux');
+const createStore = redux.createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+
+// action creator is a function that returns an action
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: "First redux action"
+    }
+}
+
+// (previousState, action) => newState
+
+const initialState = {
+    numOfCakes: 10
+}
+
+// Application State
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            };
+        default:
+            return state;
+    }
+}
+
+// store holds application state
+const store = createStore(reducer);
+
+// current state of the store
+console.log('Initial state', store.getState());
+
+// allows the app to subscribe to changes in the store
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState())); // returns the unsubscribe function
+
+// store provides dispatch method to update the state
+store.dispatch({type: BUY_CAKE}); // same
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+// unsubscribe from the store
+unsubscribe();
